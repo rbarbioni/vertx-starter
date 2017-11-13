052dc971dcd41c73dbfe98e685646329be1f39d6
rbarbioni <renan1234>
2017-11-12 22:19:44 -0200

    configure vert.x main verticle

--- src/main/java/br/com/rbarbioni/vertx/AppVerticle.java
+++ src/main/java/br/com/rbarbioni/vertx/AppVerticle.java
@@ -4,11 +4,13 @@ import br.com.rbarbioni.vertx.verticle.CamelVerticle;
 import br.com.rbarbioni.vertx.verticle.EventBusVerticle;
 import br.com.rbarbioni.vertx.verticle.JobVerticle;
 import br.com.rbarbioni.vertx.verticle.RouteVerticle;
-import io.vertx.core.Vertx;
+import io.vertx.core.AbstractVerticle;
 
-public class Application {
-  public static void main(String[] args) {
-    Vertx vertx = Vertx.vertx();
+public class AppVerticle extends AbstractVerticle {
+
+  @Override
+  public void start() throws Exception {
+    super.start();
     vertx.deployVerticle(new RouteVerticle());
     vertx.deployVerticle(new EventBusVerticle());
     vertx.deployVerticle(new JobVerticle());
